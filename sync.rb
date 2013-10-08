a892c68342dbe0abad5b3d7bb067d08166e46364
yan.cong <yan.cong@renren-inc.com>
2013-10-08 12:09:36 +0800

    0.1

--- sync.rb
+++ sync.rb
@@ -16,13 +16,6 @@ class FileMonitorObj
   end
 end
 
-# a = []
-# a1 = FileMonitorObj.new '/data/webroot/log', '/loc$/', '/.*/', '/\.log\.|\.sync_cmd_stop$/'
-# a << a1
-# File.open("sync.yaml", "w") do |io|
-#   YAML.dump(a, io)
-# end
-
 def conn cmd
   s = TCPSocket.open(@hostname, @port)
   s.puts cmd
@@ -39,7 +32,7 @@ ensure
   s.close unless s==nil
 end
 
-stop_cmd_file_name = '.sync_cmd_stop'
+stop_cmd_file_name = 'sync_cmd_stop'
 prompt_cmdline = 'ruby sync.rb start|stop|status'
 prompt_running = 'sync is running!'
 prompt_exiting = 'sync is exiting!'
@@ -58,76 +51,76 @@ $exit_flag= false;
 
 case cmd
 when 'start'
-  unless conn 'status'
-    puts prompt_starting
-    threads = []
+  exit! if conn 'status'
 
-    YAML.load_file('sync.yaml').each do |obj|
-      puts obj.absolute_path
-      puts obj.dir_disallow
-      puts obj.file_disallow
-      puts obj.file_allow
+  puts prompt_starting
+  threads = []
 
-      threads << Thread.new do
-        m = FileMonitor.new(obj.absolute_path)
-        
-        m.filter_dirs do
-          disallow /loc$/
-        end
-
-        m.filter_files do
-          disallow /.*/
-          allow /\.log\.|\.sync_cmd_stop$/
-        end
-
-        m.run do |events|
-          break if $exit_flag
-          events.each do |event|
-            if event.flags[0] == :modify
-              puts event.absolute_name
-            end
-          end
-        end
-        
-        puts 'FileMonitor thread exit!'
-      end
-    end
+  YAML.load_file('sync.yaml').each do |obj|
+    puts obj.absolute_path
+    puts obj.dir_disallow
+    puts obj.file_disallow
+    puts obj.file_allow
 
     threads << Thread.new do
-      server = TCPServer.open(@port)
+      m = FileMonitor.new(obj.absolute_path)
 
-      loop do
-        client = server.accept
-
-        cmd = client.gets
-
-        case cmd.chop
-        when 'stop'
-          client.puts(prompt_exiting)
-          $exit_flag = true;
-
-          YAML.load_file('sync.yaml').each do |obj|
-            system 'touch ' + obj.absolute_path + '/' + stop_cmd_file_name
-          end
-          sleep 1
-          YAML.load_file('sync.yaml').each do |obj|
-            system 'unlink ' + obj.absolute_path + '/' + stop_cmd_file_name
-          end
-          break;
-        when 'status'
-          client.puts(prompt_running)
-        end
-
-        puts 'client.close'
-        client.close
+      m.filter_dirs do
+        disallow /#{obj.dir_disallow}/
       end
 
-      puts 'server.close'
-      server.close unless server==nil
+      m.filter_files do
+        disallow /#{obj.file_disallow}/
+        allow /#{obj.file_allow}|#{stop_cmd_file_name}$/
+      end
+
+      m.run do |events|
+        break if $exit_flag
+        events.each do |event|
+          if event.flags[0] == :modify
+            puts event.absolute_name
+          end
+        end
+      end
+
+      puts 'FileMonitor thread exit!'
+    end
+  end
+
+  threads << Thread.new do
+    server = TCPServer.open(@port)
+
+    loop do
+      client = server.accept
+
+      cmd = client.gets
+
+      case cmd.chop
+      when 'stop'
+        client.puts(prompt_exiting)
+        $exit_flag = true;
+
+        YAML.load_file('sync.yaml').each do |obj|
+          system 'touch ' + obj.absolute_path + '/' + stop_cmd_file_name
+        end
+        sleep 1
+        YAML.load_file('sync.yaml').each do |obj|
+          system 'unlink ' + obj.absolute_path + '/' + stop_cmd_file_name
+        end
+        break;
+      when 'status'
+        client.puts(prompt_running)
+      end
+
+      puts 'client.close'
+      client.close
     end
 
-    threads.each { |t| t.join }
+    puts 'server.close'
+    server.close unless server==nil
   end
+
+  threads.each { |t| t.join }
 when /stop|status/
   puts prompt_no_running unless conn cmd
 else
